3e9f88fdfc2e78f257ac6c3dc2d65cc814e80874
Xennoxx <nicknifffe@gmail.com>
2023-02-17 22:36:07 +0100

    Wipe1

---
+++ textAdventure(thePassauDom)/Iattack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textAdventure_thePassauDom_
+{
+    internal interface Iattack
+    {
+        //Characteristics
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int GivenDamage { get; set; }
+        public int TakenDamage { get; set; }
+        public int ReducedSp { get; set; }
+        //Methods
+        public void AttackPractise() { }
+        public void AttackDescription() { }
+    }
+}
---
+++ textAdventure(thePassauDom)/Ientity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ientity
+{
+    internal class Ientity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int HP { get; set; }
+        public int Sp { get; set; }
+    }
+}
